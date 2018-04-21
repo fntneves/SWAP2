c6a247c37535456714c001bb4de77634efcaf2e5
José Luís Silva <jlsilva1994@gmail.com>
2018-04-21 18:36:52 +0100

    meti os inputs em pastas e fiz o modeloV2

--- modeloV2.py
+++ modeloV2.py
@@ -11,17 +11,17 @@ alunos = {}
 slots = {}
 presencas = {}
 
-
 # Carrega alunos e as ucs em que estao inscritos para o dicionario alunos {"A82382:[Hxxxxxx,Hxxxxx]"}
-for elem in alunos_json:
-    for al in elem:
-        if al in alunos:
-            print 'Aluno %s repetido' % al
-        for uc in elem[al]:
-            if al not in alunos:
-                alunos[al] = [uc]
-            else:
-                alunos[al] += [uc]
+for al in alunos_json:
+    if len(alunos) == int(sys.argv[3]):
+        break
+    if al in alunos:
+        print 'Aluno %s repetido' % al
+    for uc in alunos_json[al]:
+        if al not in alunos:
+            alunos[al] = [uc]
+        else:
+            alunos[al] += [uc]
 
 
 n_dia = 0
@@ -32,11 +32,17 @@ maximo_hora = 0
 for elem in horario_json: 
     for dia in elem:
         for uc in elem[dia]:
-            for codigo in uc:               
+            for codigo in uc:
                 horaI = int(uc[codigo]['HoraI'][:2])
                 horaF = int(uc[codigo]['HoraF'][:2])
                 minimo_hora = min(horaI,minimo_hora)
                 maximo_hora = max(horaF,maximo_hora)
+                # parte do codigo que adiciona o codigo-T para cadeiras com teoricas do aluno
+                if len(codigo) > 6:
+                    codigoAux = codigo[:6]
+                    for al in alunos:
+                        if codigoAux in alunos[al] and codigo not in alunos[al]:
+                            alunos[al] += [codigo]
                 if codigo not in slots:
                     slots[codigo] = {}
                 turno = uc[codigo]['Turno']
@@ -46,7 +52,10 @@ for elem in horario_json:
                     slots[codigo][turno] += [(n_dia,horaI,horaF-horaI,uc[codigo]['Capacidade'])]
     n_dia += 1
 maximo_dia = n_dia
-#pprint.pprint(slots)
+# print('alunos:')
+# pprint.pprint(alunos)
+# print('ucs e slots:')
+# pprint.pprint(slots)
 
 n_al = 0
 n_uc = 0
@@ -62,18 +71,26 @@ for al in alunos:
             for turno in slots[uc]:
                 presencas[al][uc][turno] = Int('p_%s_%s_%s' % (al,uc,turno))
 
+##################### OBJECTIVOS ######################################
+
+max_all = Sum([ presencas[al][uc][turno] for al in presencas for uc in presencas[al] for turno in presencas[al][uc] ])
+
 ##################### RESTRICOES ######################################
 
-#1 - Os valores possiveis sao 0 ou 1
+#- Os valores possiveis sao 0 ou 1
 
 values_c = [ Or(presencas[al][uc][turno] == 0, presencas[al][uc][turno] == 1) for al in presencas for uc in presencas[al] for turno in presencas[al][uc]]
 
 
-#2 - Um aluno so pode ser atribuido a um e um so turno
+#- Um aluno so pode ser atribuido a um e um so turno se for TP
 
-um_turno_c =  [ Sum([ presencas[al][uc][turno] for turno in presencas[al][uc]]) == 1 for al in presencas for uc in presencas[al]]
+um_turnoTP_c =  [ Sum([ presencas[al][uc][turno] for turno in presencas[al][uc]]) == 1 for al in presencas for uc in presencas[al] if len(uc) == 6]
 
-#3 - O numero de alocacoes para turno nao pode execeder a capacidade do mesmo
+#- Um aluno so pode ser atribuido a so turno se for T, nao sendo obrigatório
+
+um_turnoT_c =  [ Sum([ presencas[al][uc][turno] for turno in presencas[al][uc]]) <= 1 for al in presencas for uc in presencas[al] if len(uc) > 6]
+
+#- O numero de alocacoes para turno nao pode execeder a capacidade do mesmo
 
 # ZE
 lista_capacidades = {}
@@ -88,7 +105,7 @@ for al in presencas:
                 lista_capacidades[uc][turno] += [ presencas[al][uc][turno] ]
 capacidade_maxima_c = [ Sum(lista_capacidades[uc][turno]) <= slots[uc][turno][i][3] for uc in lista_capacidades for turno in lista_capacidades[uc] for i in range(len(slots[uc][turno]))]
 
-#4 Aulas praticas sem sobreposicoes
+# Aulas sem sobreposicoes
 # Percorre todos os alunos e todas as ucs e junta todos os seus turnos num dicionario do tipo de acordo com o seu dia e hora, do tipo {Aluno:{Dia:{Hora:[Ucs]}}}
 # Por ultimo, a soma dessa lista com as Ucs num determinado dia, numa determinada hora tem de ser no maximo 1, ou seja, tem de haver no maximo uma alocaçao
 
@@ -107,88 +124,114 @@ for al in presencas:
                     tuplo = slots[uc][t]
                     for i in range(len(tuplo)):
                         if tuplo[i][0] == d and (tuplo[i][1] <= h < (tuplo[i][1] + tuplo[i][2])):
-                            turnos[al][d][h] += [presencas[al][uc][t]]
+                            turnos[al][d][h] += [ presencas[al][uc][t] ]
 
-sem_sobreposicoes_c = [ Sum(turnos[al][d][h]) <= 1 for al in turnos for d in turnos[al] for h in turnos[al][d]] 
+sem_sobreposicoes_c = [ Sum(turnos[al][d][h]) <= 1 for al in turnos for d in turnos[al] for h in turnos[al][d] if len(turnos[al][d][h]) > 0 ]
 
 ########################### SOLVER ############################
 print 'Numero de alunos: %s' % len(alunos)
-s = Solver()
-s.add(values_c)
-x = time.clock()
-print 'Solving constraint 1'
-if s.check() != sat:
-    print 'Failed to solver constraint 1'
-    sys.exit()
 
+s = Optimize()
+s.maximize(max_all)
+
+x = time.clock()
+s.add(values_c)
+print 'Solving constraint 0 or 1'
+if s.check() != sat:
+    print 'Failed to solver constraint 0 or 1'
+    sys.exit()
 x1 = time.clock()
 print x1 - x
 
-s.add(um_turno_c)
-
-print 'Solving constraint 2'
+s.add(um_turnoT_c)
+print 'Solving constraint tentar um turno T'
 if s.check() != sat:
-    print 'Failed to solver constraint 2'
+    print 'Failed to solver constraint tentar um turno t'
     sys.exit()
 x = time.clock()
-
 print x - x1
 
 s.add(sem_sobreposicoes_c)
-print 'Solving constraint 3'
+print 'Solving constraint sem sobreposicções'
 if s.check() != sat:
-    print 'Failed to solver constraint 3'
+    print 'Failed to solver constraint sem sobreposicções'
+    sys.exit()
+x = time.clock()
+print x - x1
+
+s.add(capacidade_maxima_c)
+print 'Solving constraint capacidade maxima'
+if s.check() != sat:
+    print 'Failed to solver constraint capacidade maxima'
     sys.exit()
 x1 = time.clock()
 print x1 - x
 
-s.add(capacidade_maxima_c)
-
-print 'Solving constraint 4'
-
-if s.check() == sat:
-    x = time.clock()
-    print x-x1
-    m = s.model()
-    r = {}
-    for al in presencas:
-        if al not in r:
-            r[al] = {}
-        for uc in presencas[al]:
-            if uc not in r[al]:
-                r[al][uc] = {} 
-            for turno in presencas[al][uc]:
-                aloc = m.evaluate(presencas[al][uc][turno])
-                if aloc == 1:
-                    r[al][uc][turno] = aloc
-    alocacoes_finais = {}
-    
-    #alunos alocados a todas as cadeiras que estao inscritos
-    total_aloc = 0
-    n_tur = 0
-    for al in r:  
-        for u in r[al]:
-            if u not in alocacoes_finais:
-                alocacoes_finais[u] = {}
-            for t in r[al][u]:
-                if r[al][u][t] == 1:
-                    n_tur += 1
-                    if t not in alocacoes_finais[u]:
-                        alocacoes_finais[u][t] = 1
-                    else:
-                        alocacoes_finais[u][t] += 1
-        #calcular alunos alocados a todas as cadeiras que estao inscritos  
-        if n_tur == len(r[al]):
-            total_aloc += 1
-            n_tur = 0
-
-    pprint.pprint(alocacoes_finais)
-    pprint.pprint(r)
-    print 'Alunos alocados a todas as ucs: %s' % str(total_aloc)
-    print s.statistics()
-    #print s.sexpr()
+# aqui faço push para guardar o estado caso nao resultem as restrições
+s.push()
+s.add(um_turnoTP_c)
+print 'Solving constraint obrigatório um turno TP'
+if s.check() != sat:
+    print 'Failed to solver constraint obrigatório um turno TP'
+    # como falhou , aqui faço o pop
+    s.pop()
+    s.add_soft(um_turnoTP_c,100)
+    if s.check() == sat:
+        x1 = time.clock()
+        print x1-x
+        m = s.model()
+    else: 
+        print('failed to solve!') 
+        sys.exit() 
 else:
-    print "failed to solve"
+    x1 = time.clock()
+    print x1-x
+    m = s.model()
+
+r = {}
+for al in presencas:
+    if al not in r:
+        r[al] = {}
+    for uc in presencas[al]:
+        if uc not in r[al]:
+            r[al][uc] = {} 
+        for turno in presencas[al][uc]:
+            aloc = m.evaluate(presencas[al][uc][turno])
+            if aloc == 1:
+                r[al][uc][turno] = aloc
+alocacoes_finais = {}
+
+#alunos alocados a todas as cadeiras que estao inscritos
+total_aloc = 0
+n_tur = 0
+nao_alocados = []
+for al in r:  
+    for u in r[al]:
+        if u not in alocacoes_finais:
+            alocacoes_finais[u] = {}
+        for t in r[al][u]:
+            if r[al][u][t] == 1:
+                n_tur += 1
+                if t not in alocacoes_finais[u]:
+                    alocacoes_finais[u][t] = 1
+                else:
+                    alocacoes_finais[u][t] += 1
+    #calcular alunos alocados a todas as cadeiras que estao inscritos  
+    if n_tur >= len(r[al]):
+        total_aloc += 1
+        n_tur = 0
+    else:
+        #adicionar alunos não colocados nesta lista
+        nao_alocados.append(al)
+
+
+pprint.pprint(alocacoes_finais)
+# pprint.pprint(r)
+print 'Alunos alocados a todas as ucs: %s' % str(total_aloc)
+print 'Alunos não alocados: '
+pprint.pprint(nao_alocados)
+#print s.statistics()
+# print s.sexpr()
 
 # s.add(capacidade_maxima_c)
 # if s.check() == sat:
